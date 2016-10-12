01a7506ec11f680840c8024776cbcb6117e6c667
Adrian Lee <leeadrian841@gmail.com>
2016-10-12 14:08:35 +0800

    add new files

--- routes/users.js
+++ routes/users.js
@@ -7,14 +7,13 @@ router.get('/', function (req, res) {
   res.render('users/index')
 }).get('/new', function (req, res) {
   res.render('users/new')
+}).get('/:id', function (req, res) {
+  res.send('Welcome to ' + req.params.id + ' \'s posts.')
+}).get('/:id/new', function (req, res) {
+  res.send('Hi, ' + req.params.id + '!. Type your new post here.')
+}).get('/:id/edit', function (req, res) {
+  res.send('Hi, ' + req.params.id + '!. Edit your new post here.')
 })
-// .get('/:id', function (req, res) {
-//   res.send('Welcome to ' + req.params.id + ' \'s posts.')
-// }).get('/:id/new', function (req, res) {
-//   res.send('Hi, ' + req.params.id + '!. Type your new post here.')
-// }).get('/:id/edit', function (req, res) {
-//   res.send('Hi, ' + req.params.id + '!. Edit your new post here.')
-// })
 
 router.post('/', function (req, res) {
   res.send(req.body)
