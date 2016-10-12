774839058cc0b3bfaffc9b39e763870ae989615d
Adrian Lee <leeadrian841@gmail.com>
2016-10-12 12:11:27 +0800

    add new files

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,37 @@ var router = express.Router()
 // setting the route
 // app.get('/path-name', callaback(req, res))
 router.get('/', function (req, res) {
-  res.send('Welcome to Users Homepage.')
+  res.send('All users posts')
 })
-router.get('/aboutus', function (req, res) {
-  res.send('This is Users Info.')
+router.get('/:id', function (req, res) {
+  res.send('Welcome to ' + req.params.id + ' \'s posts.')
 })
-router.get('/contactus', function (req, res) {
-  res.send('I am located at Yishun.')
+router.get('/:id/new', function (req, res) {
+  res.send('Hi, ' + req.params.id + '!. Type your new post here.')
+})
+router.get('/:id/edit', function (req, res) {
+  res.send('Hi, ' + req.params.id + '!. Edit your new post here.')
+})
+router.delete('/:id', function (req, res) {
+  res.send('Hi, ' + req.params.id + '!. Do you want to delete this post?')
+})
+
+router.post('/', function (req, res) {
+  res.send('post movie form')
+})
+
+// UPDATE ROUTES
+router.get('/:id/edit', function (req, res) {
+  res.send('edit movie\'s ' + req.params.id + ' details')
+})
+
+router.put('/:id', function (req, res) {
+  res.send('edit movie' + req.params.id)
+})
+
+// DELETE ROUTES
+router.delete('/:id', function (req, res) {
+  res.send('delete movie' + req.params.id)
 })
 
 module.exports = router
